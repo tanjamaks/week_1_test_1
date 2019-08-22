ee875137761ee0b0bf2cc9da4c395513827d82ea
tanjamaks <54280137+tanjamaks@users.noreply.github.com>
2019-08-22 16:18:36 +0200

    Add files via upload
    Ovo je moj prvi php fajl. Nadam se da je uspješno komitovan.

---
+++ week_1_test_1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<h1 style="text-align:center">My first PHP </h1>
+<?php
+ echo "My first PHP! <br>";
+ echo str_word_count("My first PHP!");
+?>
+<br>
+<?php
+ echo "Hello World <br>";
+ echo strrev ("My name is Tanja Maksimovic");
+?>
+<br>
+<?php
+ echo str_replace ("World", "Tanja", "Hello World <br>");
+?>
+<h2 style="text-align:center">Arrays</h2>
+<?php
+$languages = array("Serbian","English","German","Spanish","Italian");
+
+echo "I speak " . $languages[0] . ", " . $languages[1] . ", " . $languages[2] . ", " . $languages[3] . " and " . $languages[4] . ".<br>";
+echo count($languages);
+?>
+<br>
+<h2 style="text-align:center">Variables</h2>
+
+<?php
+$x = 7;
+$y = 8.5;
+echo $x * $y;
+?>
+<br>
+<?php
+if($x<$y) {
+    echo $y - $x;
+} else {
+    echo $x + $y;
+}
+?>
+<br>
+<?php 
+$dan = date("D");
+
+if($dan == "Mon") {
+    echo "4 more days until weekend";
+} elseif ($dan == "Tue") {
+    echo "3 more days untill weekend";
+} elseif ($dan == "Wed") {
+    echo "2 more days untill weekend";
+} elseif ($dan == "Thu") {
+    echo "1 more day untill weekend";
+} else {
+    "Have a nice weekend!";
+}
+?>
+
+</body>
+</html>
