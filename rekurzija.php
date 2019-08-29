aec4a8924f39ed0ab98034548f0f0cb57dac0f22
tanjamaks <54280137+tanjamaks@users.noreply.github.com>
2019-08-29 11:46:43 +0200

    Add files via upload

---
+++ rekurzija.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Recursion Example #2: Reading a Tree of Files and Folders</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+<body>
+
+<h1>Recursion Example #2: Reading a Tree of Files and Folders</h1>
+<form action="recursive-example-3.php" method="POST">
+    <p>Pretraži željeni folder:</p> <input type="search" style="width:300px" name="unos">
+    <input type="submit" value="Pretraži">
+</form>
+<?php
+
+$folderPath = '';
+$folderPath = $_POST["unos"];
+
+
+function readFolder( $path ) {
+
+  // Open the folder
+  if ( !( $dir = opendir( $path ) ) ) {
+      die( "Can't open $path" );
+  }
+
+  $filenames = array();
+
+  // Read the contents of the folder, ignoring '.' and '..', and
+  // appending '/' to any subfolder names. Add all the files and
+  // subfolders to the $filenames array.
+
+  while ( $filename = readdir( $dir ) ) {
+    if ( $filename != '.' && $filename != '..' ) {
+      if ( is_dir( "$path/$filename" ) ) {
+          $filename .= '/';
+      }
+      $filenames[] = $filename;
+    }
+  }
+
+  closedir ( $dir );
+
+  // Sort the filenames in alphabetical order
+  sort( $filenames );
+
+  // Display the filenames, and process any subfolders
+
+  echo "<ul>";
+
+  foreach ( $filenames as $filename ) {
+    echo "<li>$filename";
+    if ( substr( $filename, -1 ) == '/' ) readFolder( "$path/" . substr( $filename, 0, -1 ) );
+    echo "</li>";
+  }
+
+  echo "</ul>";
+}
+
+echo "<h2>Contents of '$folderPath':</h2>";
+readFolder( $folderPath );
+
+?>
+
+</body>
+</html>
