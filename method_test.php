77d13edb52a193a87dff2d0378d1273a17225204
tanjamaks <tanjamaks91@gmail.com>
2019-09-02 15:36:15 +0200

    dodana skripta za testiranje HTTP metoda

---
+++ method_test.php
@@ -0,0 +1,20 @@
+<form action="method_test.php?var=value" method="POST">
+    <input name="post_var" value="value1">
+    <input type="submit">
+</form>
+
+<pre>
+
+<?php
+
+echo "<h1>GET:</h1>";
+var_dump($_GET);
+
+
+echo "<h1>POST:</h1>";
+var_dump($_POST);
+
+echo "<h1>SERVER:</h1>";
+var_dump($_SERVER);
+
+?>
